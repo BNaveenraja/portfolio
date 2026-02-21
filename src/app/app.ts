98cbea18be8e629c0543a8b438dd0417dcd9730c
BPranesh27 <bpranesh594@gmail.com>
2026-02-21 10:03:25 +0530

    feat: finalize personal details and contact section

---
+++ src/app/app.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Experience } from './components/experience/experience';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    Header,
+    Hero,
+    About,
+    Experience,
+    Projects,
+    Skills,
+    Contact,
+    Footer,
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'portfolio-web';
+}
